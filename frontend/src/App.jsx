7b0dadefeef2cabaf0ee1ac944fbc6c41f12751c
Anshu Vishwakarma <anshu.vishwakarma@mysetu.com>
2025-01-12 13:31:23 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from 'styled-components'
+import mountain from './img/mountain.jpg'
+import { MainLayout } from './styles/Layout';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <AppStyled bg={mountain}>
+    <MainLayout>
+<h1>hi</h1>
+    </MainLayout>
+    </AppStyled>
   )
 }
+const AppStyled=styled.div`
+  height:100vh;
+  background-image:url(${props=>props.bg});
+  position:relative;
+  `;
+
 
 export default App
